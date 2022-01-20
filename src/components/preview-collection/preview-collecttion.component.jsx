a40feecb5441643f41a80840adcfbe792f8b1ba5
Poom <poom@charoenyost.com>
2022-01-20 13:08:14 +0700

    update shoop data and add cart func

--- src/components/preview-collection/preview-collecttion.component.jsx
+++ src/components/preview-collection/preview-collecttion.component.jsx
@@ -9,8 +9,8 @@ const PreviewCollection = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...ohterItemProps }) => (
-          <CollectionItem key={id} {...ohterItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
